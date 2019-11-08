9bab7eaa02368cd5300ba31d94b64cca623446d6
Michal Duda <github@vookimedlo.cz>
2019-11-08 22:33:07 +0100

    transition to the Python 3

--- src/git2changelog.py
+++ src/git2changelog.py
@@ -133,27 +133,28 @@ class CLData:
         for row in data:
             r_check, release = self._format_release(row[6])
             if r_check:
-                print "\n* %s %s <%s> %s" % (
-                    self._format_date(row[1]), row[2], row[3], release)
+                print("\n* %s %s <%s> %s" % (
+                    self._format_date(row[1]), row[2], row[3], release))
                 if self.search_terms:
                     for item in self.search_terms:
                         if item in row[5]:
-                            print "- %s : Commit %s" % (row[5], row[4])
+                            print("- %s : Commit %s" % (row[5], row[4]))
                             break
                 else:
-                    print "- %s : Commit %s" % (row[5], row[4])
+                    print("- %s : Commit %s" % (row[5], row[4]))
             else:
                 if self.search_terms:  # if we want to limit the output to
                     # lines with certain strings in the comment
                     for item in self.search_terms:
                         if item in row[5]:
-                            print "- %s : Commit %s" % (row[5], row[4])
+                            print("- %s : Commit %s" % (row[5], row[4]))
                 else:  # if we want all of the commits - no search term given
-                    print "- %s : Commit %s" % (row[5], row[4])
+                    print("- %s : Commit %s" % (row[5], row[4]))
 
     def format_changelog(self):
 
         data = self._get_git_log()
-        raw_data = csv.reader(data)
+        data_str = [i.decode() for i in data]
+        raw_data = csv.reader(data_str)
         if self.log_format == 'rpm':
             self._generate_rpm_changelog(raw_data)
